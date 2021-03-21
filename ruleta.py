0d4833ab82b086b6d09259e93fff50ae0ded50f3
Milena <milenalebedin@gmail.com>
2021-03-21 17:08:14 -0300

    cambio while por operaciones de numpy arrays

--- ruleta.py
+++ ruleta.py
@@ -11,22 +11,19 @@ class Ruleta():
 
     # gira la ruleta nVeces y guarda los resultado
     def girarRuleta(self, nVeces):
-        i=1
-        while i < n:
-            numero = np.random.randint(0,nMax)
-            self.ganadores.append(numero)
-            i += 1
+        numero = np.random.randint(0, self.nroMaximo, nVeces)
+        self.ganadores.append(numero)
 
-    def graficarFrecuenciaRelativa(self):
-        #por lo que entiendo es graficar cuantas veces salio un número hasta la tirada X
+    def graficarFrecuenciaRelativa(self, nro):
+        # Cantidad de veces que salio nro / total de tiradas, hasta la tirada X
         x = np.linspace(0, 2, 100)
-        frecuencia = ganadores[0:x].count(ganador)
+        frecuencia = self.ganadores[0:x].count(nro)
         y = frecuencia / x
-
+        self.crearGrafico(x,y)
  
     def graficarMedia(self):
         x = np.linspace(0, 2, 100)
-        y = numpy.mean(self.ganadores[0:x]) # calcula la media hasta el ganador X de la ruleta
+        y = np.mean(self.ganadores[0:x]) # calcula la media hasta el ganador X de la ruleta
 
         fig, ax = plt.subplots()  # Crea una figura y ejes
         ax.plot(x, y)  # Plotear datos en los ejes
@@ -35,21 +32,26 @@ class Ruleta():
         ax.set_title("Media en funcion de cantidad de tiros")  # Titulo
         ax.legend()  # Add a legend
         plt.show() 
-
-
+        
     def graficarVarianza(self):
-        i=0
+        self.graficarFrecuenciaRelativa()
 
     def graficarDesvio(self):
-        self.graficarMedia()
+        self.graficarFrecuenciaRelativa()
         
-    ## list seria la lista q obtenemos de GIRARRULETA()
-    # yo decia ir guardando esas listas dentro de la clase (en un array, ganadores o el nombre que pinte)
+    
+     # crea todos los graficos pedidos
+    def getGraficos(self):
+        self.graficarFrecuenciaRelativa()
 
-    def getGraficos():
-        self.graficarMedia()
-        #...
-     # que haga todos los graficos y los muestre
+     def crearGrafico(x, y):
+        fig, ax = plt.subplots()  # Crea una figura y ejes
+        ax.plot(x, y)  # Plotear datos en los ejes
+        ax.set_xlabel('# jugadas')  # Titulo eje x
+        ax.set_ylabel('Media')  # Titulo eje y
+        ax.set_title("Media en funcion de cantidad de tiros")  # Titulo
+        ax.legend()  # Add a legend
+        plt.show() 
 
 
 
